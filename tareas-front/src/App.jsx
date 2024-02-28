44eacced8a4e909c9d7ad2df1b24c0549ef8e037
Fernanda López <fer.daye.2007@gmail.com>
2024-02-27 22:02:48 -0600

    Terminada primera clase 😎

--- tareas-front/src/App.jsx
+++ tareas-front/src/App.jsx
@@ -1,9 +1,25 @@
-
+import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
+import Header from './pages/components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 function App() {
 
   return (
     <>
-    hola mundo 
+    
+      <Router>
+    <div className="container">
+      <Header/>
+        <Routes>
+
+        <Route path= '/' element={<Dashboard/>}/>
+        <Route path= '/login' element={<Login/>}/>
+        <Route path= '/register' element={<Register/>}/>
+        </Routes>
+    </div>
+      </Router>
+
     </>
   )
 }
--- tareas-front/src/pages/Register.jsx
+++ tareas-front/src/pages/Register.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react"
+import {FaUser} from 'react-icons/fa'
 
 const register = () => {
+  
+  const [fromData, setFromData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = fromData
+  const onChange = (e)=>{
+    setFromData((prevState) =>({
+      ...prevState, 
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  
   return (
-    <div>register</div>
+    <>
+      <section className="heading">
+        <h3> <FaUser/> Registrar usuario </h3>
+        <p> crea un usuario </p>
+      </section>
+
+      <section className="form"> 
+        <form >
+          <div className="from-group">
+              <input 
+              type="text"
+              className="form-control"
+              id='name'
+              value={name}
+              placeholder="Ingresa tu nombre"
+              onChange={onChange}
+              />
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
