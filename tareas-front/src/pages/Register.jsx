3d8bb82f1b88a02058731986674260a11ae57505
Fernanda López <fer.daye.2007@gmail.com>
2024-03-01 12:22:32 -0600

    frontend login 🟢

--- tareas-front/src/pages/Register.jsx
+++ tareas-front/src/pages/Register.jsx
@@ -19,6 +19,9 @@ const register = () => {
     }))
   }
 
+  const onSubmit = (e) =>{
+    e.preventDefault()
+  }
   
   return (
     <>
@@ -28,18 +31,56 @@ const register = () => {
       </section>
 
       <section className="form"> 
-        <form >
-          <div className="from-group">
-              <input 
-              type="text"
-              className="form-control"
-              id='name'
-              name="name"
-              value={name}
-              placeholder="Ingresa tu nombre"
-              onChange={onChange}
-              />
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+            type="text"
+            className="form-control"
+            id='name'
+            name="name"
+            value={name}
+            placeholder="Ingresa tu nombre"
+            onChange={onChange}
+            />
           </div>
+          <div className="form-group">
+            <input 
+            type="email"
+            className="form-control"
+            id='email'
+            name="email"
+            value={email}
+            placeholder="Ingresa tu email"
+            onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+          <input 
+            type="password"
+            className="form-control"
+            id='password'
+            name="password"
+            value={password}
+            placeholder="Ingresa tu password"
+            onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+            type="password"
+            className="form-control"
+            id='password2'
+            name="password2"
+            value={password2}
+            placeholder="Confirma tu password"
+            onChange={onChange}
+            />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-block">
+                Crear
+              </button>
+            </div>
         </form>
       </section>
     </>
