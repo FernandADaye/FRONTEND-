c22bf71cacd64b87fea98f45cbae2bd778e312c4
Fernanda López <fer.daye.2007@gmail.com>
2024-03-01 12:45:34 -0600

    frontend login 🟢

--- tareas-front/src/pages/Login.jsx
+++ tareas-front/src/pages/Login.jsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from "react"
+import {FaSignInAlt} from 'react-icons/fa'
 
 const login = () => {
+
+  const [fromData, setFromData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password, } = fromData
+
+  const onChange = (e)=>{
+    setFromData((prevState) =>({
+      ...prevState, 
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+  }
+  
+
   return (
-    <div>login</div>
-  )
+    <>
+      <section className="heading">
+    <h3> <FaSignInAlt/> Ingresa tu Usuario</h3>
+    <p> Pon tus credenciales </p>
+  </section>
+  <section className="form"> 
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <input 
+        type="email"
+        className="form-control"
+        id='email'
+        name="email"
+        value={email}
+        placeholder="Ingresa tu email"
+        onChange={onChange}
+        />
+      </div>
+      <div className="form-group">
+      <input 
+        type="password"
+        className="form-control"
+        id='password'
+        name="password"
+        value={password}
+        placeholder="Ingresa tu password"
+        onChange={onChange}
+        />
+      </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-block">
+            Siguiente
+          </button>
+        </div>
+    </form>
+  </section>
+    </>
+    )
 }
 
 export default login
