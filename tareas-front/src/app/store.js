584fac2c29f46e0b5b23899af29084a385fe92c6
Fernanda López <fer.daye.2007@gmail.com>
2024-03-02 12:32:43 -0600

    👌🏻 funciona

--- tareas-front/src/app/store.js
+++ tareas-front/src/app/store.js
@@ -1,7 +1,7 @@
 //  aqui es donde se amacenaran todas as rebanadas (todos los Slices)
 // importar unas cosas y tambien importar el slice 
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer } from "../features/auth/authSlice";
+import authReducer  from "../features/auth/authSlice";
 
 // se va a importar Store junto con un metodo de configuracion, el cual es un objeto 
 export const store = configureStore({
