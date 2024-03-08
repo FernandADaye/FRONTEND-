740e628c1c131a89df6c2bc0c95dc1d712c7de88
Fernanda López <fer.daye.2007@gmail.com>
2024-03-08 13:59:19 -0600

    mas y mas codigo

--- tareas-front/src/features/tareas/tareasSlice.js
+++ tareas-front/src/features/tareas/tareasSlice.js
@@ -1,24 +1,43 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState={
-    taras:[],
-    isError: false ,
-    isSuccess: false, 
-    isLoading:false, 
-    message:''
-}
-export const tareaSlice = createSlice({
-    name :'tarea',
-    initialState,
-    reducer:{
-        reset: (state)=>initialState
+const initialState = {
+  taras: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
 
-    },
-    extraReducers: ()=> {
-
+//  crear una tarea
+export const crearTarea = createAsyncThunk(
+  "tareas/crear",
+  async (tareaData, thunkAPI) => {
+    try {
+      // tambien recuerda que paa ingresar una tarea debes estar logeado, osea que tengas el token, asi que le bedemos pasar que es un token y que lo reconozca
+      const token = thunkAPI.getState().auth.user.token
+      // cuando todo este bien nos retornara la funcion de services (tambien usa el token que sacamos)
+      return await tareaService.crearTarea(tareaData, token);
+    } catch (error) {
+      const menssage =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.menssage ||
+        error.toString();
+      return thunkAPI.rejectWithValue(menssage);
     }
-})
+  }
+);
 
-export const {reset} = tareaSlice.actions
+export const tareaSlice = createSlice({
+  name: "tarea",
+  initialState,
+  reducer: {
+    reset: (state) => initialState,
+  },
+  extraReducers: () => {},
+});
+
+export const { reset } = tareaSlice.actions;
 
 export default tareaSlice.reducer;
